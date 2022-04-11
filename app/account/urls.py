01caf489f03b33bc7ac6c0435f31541f8e555519
Djaizansi <youcef.jallali@gmail.com>
2022-04-12 00:55:52 +0200

    Home professional

--- app/account/urls.py
+++ app/account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('pro/', views.account_pro),
-    path('client/', views.account_client)
+    path('pro/', views.account_pro, name='account_pro'),
+    path('client/', views.account_client, name='account_client'),
 ]
