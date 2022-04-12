197d842790b6f4746385d4651b107542ab9cf69c
Marwane <manbou92@hotmail.fr>
2022-04-12 14:44:44 +0200

    booking system

--- app/account/models.py
+++ app/account/models.py
@@ -5,12 +5,3 @@ class Services(models.Model):
     description = models.CharField(max_length=255, verbose_name='description')
     user = models.ForeignKey('register.User', on_delete=models.CASCADE)
 # Create your models here.
-
-class Booking(models.Model):
-    user = models.ForeignKey('register.User', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=100)
-    date = models.DateField()
-    time = models.TimeField()
-
-    def __str__(self):
-        return self.name
