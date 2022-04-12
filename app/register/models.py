7a171bde20b8526236db3587cb027201acee3155
Djaizansi <52085560+Djaizansi@users.noreply.github.com>
2022-04-12 19:13:25 +0200

    Add update system practicien (#9)

--- app/register/models.py
+++ app/register/models.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
@@ -14,4 +15,8 @@ class User(AbstractUser):
     )
 
     role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
+    addressCabinet = models.CharField(max_length=255, verbose_name='Adresse du cabinet', null=True, default=None, blank=True)
+    phoneCabinet = PhoneNumberField(blank=True, null=True, verbose_name='Téléphone du cabinet')
+    age = models.IntegerField(verbose_name='Age', null=True, default=None, blank=True)
+    description = models.TextField(verbose_name='Description', null=True, default=None, blank=True)
 
