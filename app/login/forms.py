81bd8ecdff5b3f1b8957e84a71feb1357c804db7
12Wass <33042233+12Wass@users.noreply.github.com>
2022-04-12 17:50:12 +0200

    Implementing Connection and Registering form + stylizing using Tailwind (#7)

--- app/login/forms.py
+++ app/login/forms.py
@@ -2,5 +2,9 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Nom d’utilisateur')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+    username = forms.CharField(max_length=63, label='Nom d’utilisateur', widget=forms.TextInput(
+        attrs={'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker'}
+    ))
+    password = forms.CharField(max_length=63, label='Mot de passe', widget=forms.PasswordInput(
+        attrs={'class': 'shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3'}
+    ))
